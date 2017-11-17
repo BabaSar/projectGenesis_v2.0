71494745fd084eeb28d4b7cba8ce41a417eac6aa
Baba <baba.sariffodeen.1@gmail.com>
2017-11-17 20:11:33 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+// A function to immediately get all input elements from the form
+
+var providerIdInput;
+
+var licenseYearInput;
+var licenseMonthInput;
+var licenseDayInput;
+var licenseDurationInput;
+
+var offerYearInput;
+var offerMonthInput;
+var offerDayInput;
+var offerDurationInput;
+
+var offerDropDownInput;
+
+var serviceKeyInput;
+
+var epgYearInput;
+var epgMonthInput;
+var epgDayInput;
+var epgHoursInput;
+var epgMinutesInput;
+var epgSecondsInput;
+var randomizeEpgButton;
+
+var generateButton;
+
+var resultsTextBox;
+
+var getElements = function(){
+
+    providerIdInput = document.getElementById("providerIdInput");
+
+    licenseYearInput = document.getElementById("licenseYearInput");
+    licenseMonthInput = document.getElementById("licenseMonthInput");
+    licenseDayInput = document.getElementById("licenseDayInput");
+    licenseDurationInput = document.getElementById("licenseDurationInput");
+
+    offerYearInput = document.getElementById("offerYearInput");
+    offerMonthInput = document.getElementById("offerMonthInput");
+    offerDayInput = document.getElementById("offerDayInput");
+    offerDurationInput = document.getElementById("offerDurationInput");
+
+    offerDropDownInput = document.getElementById("offerDropDownInput");
+    serviceKeyInput = document.getElementById("serviceKeyInput");
+
+    epgYearInput = document.getElementById("epgYearInput");
+    epgMonthInput = document.getElementById("epgMonthInput");
+    epgDayInput = document.getElementById("epgDayInput");
+    epgHoursInput = document.getElementById("epgHoursInput");
+    epgMinutesInput = document.getElementById("epgMinutesInput");
+    epgSecondsInput = document.getElementById("epgSecondsInput");
+    randomizeEpgButton = document.getElementById("randomizeEpgButton");
+
+    generateButton = document.getElementById("generateButton");
+
+    resultsTextBox = document.getElementById("resultsTextBox");
+
+};
+
+getElements();
+
